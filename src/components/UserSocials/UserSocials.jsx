a8805edb5f25307ce18d36af2a9a03ec98f153e2
Arnaud Cavalletto <arnaudcavalletto@yahoo.com>
2023-02-09 17:06:44 -0700

    Started work on socials for onboarding

--- src/components/UserSocials/UserSocials.jsx
+++ src/components/UserSocials/UserSocials.jsx
@@ -1,35 +1,43 @@
-import './UserSocials.css'
+import "./UserSocials.css";
 
 const UserSocials = ({ onUpdateField }) => {
-    function handleClick(e) {
-        e.preventDefault()
-        document.querySelector("#linkedinBtn").style.visibility = "hidden";
-        document.querySelector("#linkedin").style.visibility = "visible";
-    }
-    function textFocusOut(e){
-        e.preventDefault()
-        document.querySelector("#linkedin").style.visibility = "hidden";
-        document.querySelector("#linkedinBtn").style.visibility = "visible";
-    }
+  function handleClick(e) {
+    e.preventDefault();
+    document.querySelector("#linkedinBtn").style.visibility = "hidden";
+    document.querySelector("#linkedin").style.visibility = "visible";
+  }
+  function textFocusOut(e) {
+    e.preventDefault();
+    document.querySelector("#linkedin").style.visibility = "hidden";
+    document.querySelector("#linkedinBtn").style.visibility = "visible";
+  }
   return (
     <div className="container">
-    <h5 className="mt-5"> We're almost done, promise!<br/>Let's connect...</h5>
-    <form className="mt-4" id="socialsForm">
+      <h5 className="mt-5">
+        {" "}
+        We're almost done, promise!
+        <br />
+        Let's connect...
+      </h5>
+      <form className="mt-4" id="socialsForm">
         <div id="linkedinDiv">
-            <button id="linkedinBtn" onClick = {handleClick}><i className="fa-brands fa-linkedin"></i></button>        
-            <input
-                type="text"
-                name="linkedin"
-                id="linkedin"
-                className="rounded"
-                onChange={onUpdateField}
-                onBlur={textFocusOut}
-                placeholder="LinkedIn"
-                style={{ visibility: "hidden"}}/>
+          <button id="linkedinBtn" onClick={handleClick}>
+            <i className="fa-brands fa-linkedin"></i>
+          </button>
+          <input
+            type="text"
+            name="linkedin"
+            id="linkedin"
+            className="rounded"
+            onChange={onUpdateField}
+            onBlur={textFocusOut}
+            placeholder="LinkedIn"
+            style={{ visibility: "hidden" }}
+          />
         </div>
-    </form>
-      </div>
-  )
-}
+      </form>
+    </div>
+  );
+};
 
-export default UserSocials
+export default UserSocials;
