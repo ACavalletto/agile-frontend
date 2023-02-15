5dde250f3bdf14760979a686aa82ee5203362432
masomaster <masonlancaster@protonmail.com>
2023-02-15 10:06:36 -0800

    fixes new project link

--- src/components/PageBottomButton/PageBottomButton.jsx
+++ src/components/PageBottomButton/PageBottomButton.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import "./PageBottomButton.css";
 
-const PageBottomButton = ({ buttonText }) => {
+const PageBottomButton = ({ buttonText, link }) => {
     return (
-        <Link to="/">
+        <Link to={link}>
             <div className="container d-flex flex-wrap justify-content-center align-items-center" id="pageBottomButton">
                 <p>{ buttonText }</p>
             </div>
--- src/pages/ProjectList/ProjectList.jsx
+++ src/pages/ProjectList/ProjectList.jsx
@@ -6,9 +6,9 @@ const ProjectList = ({ user, setUser }) => {
     return (
         <div>
             <h1>ProjectList</h1>
-            <Link to="/projects/new">Home</Link>
+            <Link to="/">Home</Link>
             <button onClick={() => { setUser(false) }}>Logout</button>
-            <PageBottomButton buttonText="+ Add New Project" />
+            <PageBottomButton buttonText="+ Add New Project" link="/projects/new" />
         </div>
     )
 }
