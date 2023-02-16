169008325ec684cc4dcff6b11029b93d28f6dd07
masomaster <masonlancaster@protonmail.com>
2023-02-16 09:27:33 -0800

    separates project form to separate component

--- src/components/PageBottomButton/PageBottomButton.jsx
+++ src/components/PageBottomButton/PageBottomButton.jsx
@@ -2,13 +2,13 @@ import { Link } from "react-router-dom";
 import "./PageBottomButton.css";
 
 const PageBottomButton = ({ buttonText, link }) => {
-    return (
-        <Link to={link}>
-            <div className="container d-flex flex-wrap justify-content-center align-items-center" id="pageBottomButton">
-                <p>{ buttonText }</p>
-            </div>
-        </Link>
-    )
+  return (
+    <Link to={link || ""}>
+      <div className="container d-flex flex-wrap justify-content-center align-items-center" id="pageBottomButton">
+        <p>{ buttonText }</p>
+      </div>
+    </Link>
+  )
 }
 
 export default PageBottomButton;
