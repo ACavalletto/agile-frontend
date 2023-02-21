36128b6ac63325802289b04bc262d2ba7b880581
masomaster <masonlancaster@protonmail.com>
2023-02-21 15:04:27 -0800

    prepares project detail page

--- src/pages/ProjectDetail/ProjectDetail.jsx
+++ src/pages/ProjectDetail/ProjectDetail.jsx
@@ -1,12 +1,11 @@
 import { Link } from "react-router-dom";
 
-const ProjectDetail = ({ user, setUser }) => {
+const ProjectDetail = ({ user }) => {
 
     return (
         <div>
             <h1>ProjectDetail</h1>
             <Link to="/">Home</Link>
-            <button onClick={() => { setUser(false) }}>Logout</button>
         </div>
     )
 }
