a7b2661f4820b1ae98ed9d2e5139d4242a89abc2
masomaster <masonlancaster@protonmail.com>
2023-02-21 14:51:26 -0800

    completes frontend project creation task flow

--- src/pages/NewProject/NewProject.jsx
+++ src/pages/NewProject/NewProject.jsx
@@ -6,6 +6,7 @@ import TimelineForm from "../../components/TimelineForm/TimelineForm";
 import PageBottomButton from "../../components/PageBottomButton/PageBottomButton";
 
 function NewProject (user){
+  console.log(user)
   const [formToggle, setFormToggle] = useState(true);
   const [projectInfo, setProjectInfo] = useState({
     title:"",
@@ -40,10 +41,15 @@ function NewProject (user){
     },
     roles:[""],
     categories:[""],
-    gitHubLink: "",
-    zoomLink: "",
     figmaLink: "",
-    otherLink: "",
+    gitHubFrontendLink: "",
+    gitHubBackendLink: "",
+    googleDriveLink: "",
+    jiraLink: "",
+    microsoftTeamsLink: "",
+    slackLink: "",
+    trelloLink: "",
+    zoomLink: "",
   })
 
   function handleToggle() {
@@ -51,6 +57,32 @@ function NewProject (user){
   }
   function handleChange({ currentTarget: input}) {
     setProjectInfo({...projectInfo, [input.name]: input.value})
+  }
+  function handleTimelineChange({ currentTarget: input}) {
+    const tempProjInfo = {...projectInfo};
+    tempProjInfo.timeline[input.name.slice(0, 6)][input.name.slice(7)] = input.value;
+    setProjectInfo(tempProjInfo);
+  }
+  function handleSubmit() {
+    console.log(projectInfo)
+
+    // Formats data object for Python model
+    const formData = {...projectInfo};
+    formData.profile = user.uid;
+    formData.topics = [...formData.categories];
+    formData.project_members = [...formData.projMembers];
+    formData.figma_link = formData.figmaLink;
+    formData.gitub_frontend_link = formData.gitHubFrontendLink;
+    formData.gitub_backend_link = formData.gitHubBackendLink;
+    formData.google_drive_link = formData.googleDriveLink;
+    formData.jira_link = formData.jiraLink;
+    formData.microsoft_teams_link = formData.microsoftTeamsLink;
+    formData.slack_link = formData.slackLink;
+    formData.trello_link = formData.trelloLink;
+    formData.zoom_link = formData.zoomLink;
+    console.log(formData);
+
+    // Sends projectInfo to projects POST route (once said route is created)
   }
 
   return (
@@ -64,10 +96,10 @@ function NewProject (user){
         </>
       :
         <>
-          <TimelineForm projectInfo={projectInfo} setProjectInfo={setProjectInfo} handleChange={handleChange} />
+          <TimelineForm projectInfo={projectInfo} setProjectInfo={setProjectInfo} handleTimelineChange={handleTimelineChange} />
           <Link to="" onClick={handleToggle}>Go Back</Link>
           <br />
-          <Link to="/projects">Save New Project!</Link> {/* This route needs to be updated to specific project id route */}
+          <Link to="/projects" onClick={handleSubmit} >Save New Project!</Link> {/* This route needs to be updated to specific project id route */}
         </>
       }
 
