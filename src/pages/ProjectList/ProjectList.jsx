13cff270738b72dcd8710a17751f3d666aef7d22
masomaster <masonlancaster@protonmail.com>
2023-02-24 16:56:51 -0800

    gets basic project info on the page with API

--- src/pages/ProjectList/ProjectList.jsx
+++ src/pages/ProjectList/ProjectList.jsx
@@ -1,14 +1,33 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 import PageBottomButton from "../../components/PageBottomButton/PageBottomButton";
+import * as projectsAPI from "../../utilities/projects-api";
 
-const ProjectList = ({ user, setUser }) => {
+const ProjectList = () => {
+  const [projects, setProjects] = useState(null);
 
-    return (
-        <div>
-            <h1>ProjectList</h1>
-            <PageBottomButton buttonText="+ Add New Project" link="/projects/new" />
-        </div>
+  useEffect(function() {
+    if (!projects) {
+      (async function getProjects() {
+        const allProjects = await projectsAPI.getAllProjects();
+        console.log(allProjects);
+        setProjects(allProjects);
+      })()
+    }
+  })
+
+  return (
+    projects ? (
+      <div>
+        <h1>Find A Project!</h1>
+        {projects.map(p => 
+          <div>{p.title}</div>
+        )}
+        <PageBottomButton buttonText="+ Add New Project" link="/projects/new" />
+      </div>
+    ) : (
+      <div>Loading Projects...</div>
     )
+  )
 }
 
 export default ProjectList;
