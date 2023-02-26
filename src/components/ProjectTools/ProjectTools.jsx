52a709e8aff3dd38908817b1775d995e2f766f1c
masomaster <masonlancaster@protonmail.com>
2023-02-25 19:33:10 -0800

    makes consistent naming convention for icons

--- src/components/ProjectTools/ProjectTools.jsx
+++ src/components/ProjectTools/ProjectTools.jsx
@@ -4,9 +4,9 @@ const ProjectTools = ({ project }) => {
   console.log(project.tech)
 
   // Need to write something to lookup the proper icon
-
-  // Need to write something in case user added a tech for which we don't have icon
   
+  // Need to write something in case user added a tech for which we don't have icon
+
   return (
     <div>ProjectTools</div>
   )
