8c44134144da300c34b371b12bf10234b28942ab
masomaster <masonlancaster@protonmail.com>
2023-02-28 16:30:50 -0800

    hi-fi styles NewProject creation flow

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,15 +1,23 @@
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import { logout } from "../../services/firebase";
+import "./NavBar.css";
+import genericUserPhoto from "../../images/genericUserPhoto/genericUserPhoto.png";
+import launchPadLogo from "../../images/launch-pad-logos/title_logo_72.png";
 
 const NavBar = () => {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar collapseOnSelect expand="lg" variant="light" id="navbar">
       <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <img src={launchPadLogo} />
+      <div className="userPhoto">
+        <img src={genericUserPhoto} />
+      </div>
       <Navbar.Collapse id="responsive-navbar-nav">
         <Nav className="mr-auto">
           <Nav.Link href="/">Dashboard</Nav.Link>
           <Nav.Link href="/projects">Find Projects</Nav.Link>
           <Nav.Link href="/profile">Profile</Nav.Link>
+          <Nav.Link href="/">Settings</Nav.Link>
           <Nav.Link href="/" onClick={() => {
             logout();
           }}>Logout</Nav.Link>
--- src/components/TimelineForm/TimelineForm.jsx
+++ src/components/TimelineForm/TimelineForm.jsx
@@ -3,150 +3,165 @@ import "./TimelineForm.css";
 function TimelineForm({ projectInfo, setProjectInfo, handleTimelineChange, handleToggle, handleSubmit, editToggle }) {
 
   return (
-    <div className="timeline-form">
-      <h5>Build Timeline</h5>
-      <h6>Define the 6 stages of Your Project</h6>
-      <div className="stage-field" id="stage1">
-        <h6>1.</h6>
-        <div className="name">
-          <input
-            display="none"
-            type="text" 
-            placeholder="Stage 1 Name"
-            name="stage1.name"
-            onChange={handleTimelineChange}
-            value={projectInfo.timeline.stage1.name}
-            required
-            />
+    <div className="project-info-form timeline-form">
+      <form>
+        <h4>Build Timeline</h4>
+        <h6 id="subtitle">Define the 6 stages of Your Project</h6>
+        <div className="stage-field d-flex" id="stage1">
+          <h6>1.</h6>
+          <div className="input-fields">
+            <div className="name">
+              <input
+                display="none"
+                type="text" 
+                placeholder="Stage 1 Name"
+                name="stage1.name"
+                onChange={handleTimelineChange}
+                value={projectInfo.timeline.stage1.name}
+                required
+                />
+            </div>
+            <div className="description">
+              <textarea
+                placeholder="Brief Description"
+                name="stage1.description"
+                onChange={handleTimelineChange}
+                value={projectInfo.timeline.stage1.description}
+                required
+              />
+            </div>
+          </div>
         </div>
-        <div className="description">
-          <textarea
-            placeholder="Brief Description"
-            name="stage1.description"
-            onChange={handleTimelineChange}
-            value={projectInfo.timeline.stage1.description}
-            required
-          />
+        <div className="stage-field d-flex" id="stage2">
+          <h6>2.</h6>
+          <div className="input-fields">
+            <div className="name">
+              <input
+                display="none"
+                type="text" 
+                placeholder="Stage 2 Name"
+                name="stage2.name"
+                onChange={handleTimelineChange}
+                value={projectInfo.timeline.stage2.name}
+                required
+                />
+            </div>
+            <div className="description">
+              <textarea
+                placeholder="Brief Description"
+                name="stage2.description"
+                onChange={handleTimelineChange}
+                value={projectInfo.timeline.stage2.description}
+                required
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="stage-field" id="stage2">
-        <h6>2.</h6>
-        <div className="name">
-          <input
-            display="none"
-            type="text" 
-            placeholder="Stage 2 Name"
-            name="stage2.name"
-            onChange={handleTimelineChange}
-            value={projectInfo.timeline.stage2.name}
-            required
-            />
+        <div className="stage-field d-flex" id="stage3">
+          <h6>3.</h6>
+          <div className="input-fields">
+            <div className="name">
+              <input
+                display="none"
+                type="text" 
+                placeholder="Stage 3 Name"
+                name="stage3.name"
+                onChange={handleTimelineChange}
+                value={projectInfo.timeline.stage3.name}
+                required
+                />
+            </div>
+            <div className="description">
+              <textarea
+                placeholder="Brief Description"
+                name="stage3.description"
+                onChange={handleTimelineChange}
+                value={projectInfo.timeline.stage3.description}
+                required
+              />
+            </div>
+          </div>
         </div>
-        <div className="description">
-          <textarea
-            placeholder="Brief Description"
-            name="stage2.description"
-            onChange={handleTimelineChange}
-            value={projectInfo.timeline.stage2.description}
-            required
-          />
+        <div className="stage-field d-flex" id="stage1">
+          <h6>4.</h6>
+          <div className="input-fields">
+            <div className="name">
+              <input
+                display="none"
+                type="text" 
+                placeholder="Stage 4 Name"
+                name="stage4.name"
+                onChange={handleTimelineChange}
+                value={projectInfo.timeline.stage4.name}
+                required
+                />
+            </div>
+            <div className="description">
+              <textarea
+                placeholder="Brief Description"
+                name="stage4.description"
+                onChange={handleTimelineChange}
+                value={projectInfo.timeline.stage4.description}
+                required
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="stage-field" id="stage3">
-        <h6>3.</h6>
-        <div className="name">
-          <input
-            display="none"
-            type="text" 
-            placeholder="Stage 3 Name"
-            name="stage3.name"
-            onChange={handleTimelineChange}
-            value={projectInfo.timeline.stage3.name}
-            required
-            />
+        <div className="stage-field d-flex" id="stage5">
+          <h6>5.</h6>
+          <div className="input-fields">
+            <div className="name">
+              <input
+                display="none"
+                type="text" 
+                placeholder="Stage 5 Name"
+                name="stage5.name"
+                onChange={handleTimelineChange}
+                value={projectInfo.timeline.stage5.name}
+                required
+                />
+            </div>
+            <div className="description">
+              <textarea
+                placeholder="Brief Description"
+                name="stage5.description"
+                onChange={handleTimelineChange}
+                value={projectInfo.timeline.stage5.description}
+                required
+              />
+            </div>
+          </div>
         </div>
-        <div className="description">
-          <textarea
-            placeholder="Brief Description"
-            name="stage3.description"
-            onChange={handleTimelineChange}
-            value={projectInfo.timeline.stage3.description}
-            required
-          />
+        <div className="stage-field d-flex" id="stage6">
+          <h6>6.</h6>
+          <div className="input-fields">
+            <div className="name">
+              <input
+                display="none"
+                type="text" 
+                placeholder="Stage 6 Name"
+                name="stage6.name"
+                onChange={handleTimelineChange}
+                value={projectInfo.timeline.stage6.name}
+                required
+                />
+            </div>
+            <div className="description">
+              <textarea
+                placeholder="Brief Description"
+                name="stage6.description"
+                onChange={handleTimelineChange}
+                value={projectInfo.timeline.stage6.description}
+                required
+              />
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="stage-field" id="stage1">
-        <h6>4.</h6>
-        <div className="name">
-          <input
-            display="none"
-            type="text" 
-            placeholder="Stage 4 Name"
-            name="stage4.name"
-            onChange={handleTimelineChange}
-            value={projectInfo.timeline.stage4.name}
-            required
-            />
+        <div className="buttons">
+          <input type="submit" className="purple-bg" id="firstButton" value=" << Go Back " onClick={handleToggle} />
+          <input type="submit" className="purple-bg" id="secondButton" value={editToggle ? " Update Timeline >> " : " Save New Timeline >> "} onClick={handleSubmit} />
         </div>
-        <div className="description">
-          <textarea
-            placeholder="Brief Description"
-            name="stage4.description"
-            onChange={handleTimelineChange}
-            value={projectInfo.timeline.stage4.description}
-            required
-          />
-        </div>
-      </div>
-      <div className="stage-field" id="stage5">
-        <h6>5.</h6>
-        <div className="name">
-          <input
-            display="none"
-            type="text" 
-            placeholder="Stage 5 Name"
-            name="stage5.name"
-            onChange={handleTimelineChange}
-            value={projectInfo.timeline.stage5.name}
-            required
-            />
-        </div>
-        <div className="description">
-          <textarea
-            placeholder="Brief Description"
-            name="stage5.description"
-            onChange={handleTimelineChange}
-            value={projectInfo.timeline.stage5.description}
-            required
-          />
-        </div>
-      </div>
-      <div className="stage-field" id="stage6">
-        <h6>6.</h6>
-        <div className="name">
-          <input
-            display="none"
-            type="text" 
-            placeholder="Stage 6 Name"
-            name="stage6.name"
-            onChange={handleTimelineChange}
-            value={projectInfo.timeline.stage6.name}
-            required
-            />
-        </div>
-        <div className="description">
-          <textarea
-            placeholder="Brief Description"
-            name="stage6.description"
-            onChange={handleTimelineChange}
-            value={projectInfo.timeline.stage6.description}
-            required
-          />
-        </div>
-      </div>
-      <button onClick={handleToggle}>Go Back</button>
-      <br />
-      <button onClick={handleSubmit}>{editToggle ? "Update Timeline" : "Save New Timeline"}</button>
+      </form>
     </div>
   )
 }
